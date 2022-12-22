dd2568866209ed533963ce8e2ad8300e62c20e24
fisher <fisheralexandr@gmail.com>
2022-12-22 21:38:25 +0400

    min max

--- stream/methods/MyReduce.java
+++ stream/methods/MyReduce.java
@@ -13,6 +13,10 @@ public class MyReduce {
 
         System.out.println(reduce);
 
+        /** Найти максимальное или минимальное значение в списке */
+        Integer max = Arrays.asList(1, 2, 3, 4, 5, 6).stream()
+                .reduce(0, Integer::max);
+
         /** Посчитать общую каллорийность всего меню */
         Integer totalCcal = Dish.menu.stream()
                 .map(Dish::calories)
