25175e31fa90a1880af8583ef9cc0c625d8fa24a
fisher <fisheralexandr@gmail.com>
2022-12-27 00:56:23 +0400

    groupingBy

--- stream/MyStream.java
+++ stream/MyStream.java
@@ -5,6 +5,7 @@ import func_interfaces.model.Dish;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 public class MyStream {
     public static List<Dish> menu = Arrays.asList(
@@ -18,11 +19,11 @@ public class MyStream {
             new Dish("prawns", false, 300, Dish.Type.FISH),
             new Dish("salmon", false, 450, Dish.Type.FISH) );
 
-
     public static void main(String[] args) {
-
-        /** Intermediate operations */
-        long count = menu.stream()
+        /**
+         * Intermediate operations
+         */
+        long dishCount = menu.stream()
                 .filter(d -> d.calories() > 100) // отбираем блюда калорийностью больше 100
                 .peek(System.out::println) // подглядываем в поток, и выводим в консоль элементы
                 .sorted(Comparator.comparingInt(Dish::calories)) // сортируем по каллориям
@@ -41,5 +42,34 @@ public class MyStream {
                 .flatMap(i -> list2.stream()  // сливаем 2 потока в один
                         .map(j -> i * j))
                 .forEach(i -> System.out.print(i + ", "));
+
+
+        /**
+         * Terminal Operations
+         * */
+        menu.stream().forEach(System.out::println); // пробегаем по каждому элементу
+        long count =
+                menu.stream().count(); // считаем количество элементов
+        boolean allBeef =
+                menu.stream().allMatch(d -> d.name().equals("beef")); // проверяем все ли названия блюд соответствуют "beef"
+        boolean anyBeef =
+                menu.stream().anyMatch(d -> d.name().equals("beef")); // проверяем хоть одно название соответствует "beef"
+        boolean beef =
+                menu.stream().noneMatch(d -> d.name().equals("beef")); // проверяем, что ни одно название не соответствует "beef"
+        Optional<Dish> first =
+                menu.stream().findFirst(); // берем первый элемент
+        Optional<Dish> any =
+                menu.stream().findAny(); // берем любой элемент
+
+        // reduce - уменьшать, сводить
+        // В аргументах указываем: начальное значение и лямбду, которая группирует текущий и следующий элемент. */
+        Integer sum = menu.stream()
+                .map(Dish::calories)
+                .reduce(0, (a, b) -> a + b); // найдем сумму каллаорий всей блюд
+
+        // вариант reduce без начального значения возвращает Optional
+        Optional<Integer> reduce = menu.stream()
+                .map(Dish::calories)
+                .reduce(Integer::sum); // здесь я использовал еще вместо обычной лямбды ссылку на суммирование
     }
 }
